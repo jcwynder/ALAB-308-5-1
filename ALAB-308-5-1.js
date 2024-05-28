d5cab577b0a622125feb3eb2507f736ace455330
Jermal Wynder <jermalwynder@gmail.com>
2024-05-28 15:09:07 -0400

    second solution

--- ALAB-308-5-1.js
+++ ALAB-308-5-1.js
@@ -8,7 +8,17 @@ function sumArray(array) {
   }
   return total;
 }
+// Takes an array of numbers and returns the average
+function averageArray(array) {
+  var total = 0;
+  for (i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total / array.length;
+}
 
 var numbers = [1, 2, 3, 4, 5];
 var result = sumArray(numbers);
+var result2 = averageArray(numbers);
 console.log(result);
+console.log(result2);
